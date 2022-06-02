0b163d945bcbb74b8fcf736a4883aa69cd1bc8db
Oliver Ye <oliverye@berkeley.edu>
2022-06-02 13:15:49 -0700

    proj2 initialize

--- test/src/index.js
+++ test/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
 
 
 /**
@@ -27,7 +25,6 @@ function Bruh() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App />
   </div>
 );
 
