ed84709cdef1de32c80bb2cad5ee5211df80f2cf
Oliver Ye <oliverye@berkeley.edu>
2022-06-06 01:09:32 -0700

    fixed border

--- test/src/App.js
+++ test/src/App.js
@@ -4,7 +4,7 @@ import headshot from './assets/headshot.jpg';
 
 function App() {
   return (
-    <div className="container mx-auto bg-[#F5F5F5] rounded-xl border">
+    <div className="container mx-auto bg-[#F5F5F5] rounded-xl">
       <img src={headshot} className="rounded-xl"></img>
       <Main />
       <Footer />
