f7d39e1fc1707cc974f00a826712c105b629c4e3
Oliver Ye <oliverye@berkeley.edu>
2022-06-10 15:03:53 -0700

    done

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,29 @@
 import Location from "./Location";
 import Navbar from "./Navbar";
+import data from './data';
+import archery from './assets/archery.jpg';
+import cooking from './assets/cooking.jpg';
+import ski from './assets/ski.jpg';
+import zion from './assets/zion.jpeg';
+
 function App() {
+    const cards = data.map(item => {
+        return (
+            <Location
+                key={item.id}
+                title={item.title}
+                img={item.img}
+                location={item.location}
+                tag={item.tag}
+                dscrp={item.description}
+                link={item.link}
+            />
+        )
+    })
     return (
         <div>
             <Navbar />
-            <Location />
+            {cards}
         </div>
     );
 }
--- test/src/Location.js
+++ test/src/Location.js
@@ -1,34 +1,32 @@
-import archery from './assets/archery.jpg';
 import pin from './assets/pin.png';
 
 function Location(props) {
     return (
-        <div className="mt-5">
+        <div className="mt-5 block ml-40">
             <div className="flex">
-                <img src={archery} className="object-cover h-96 w-72 rounded-2xl"></img>
-                <div className="my-10 ml-6 font-Poppins w-1/2">
+                <img src={props.img} className="object-cover h-96 w-72 rounded-2xl"></img>
+                <div className="my-10 ml-6 font-Poppins w-1/2 mr-40">
                     <div className="flex">
                         <div className="inline-block align-middle">
                             <img src={pin} alt="location-pin" className="inline-block align-middle h-6"></img>
                             <div className="ml-3 inline-block align-middle tracking-widest">
-                                BERKELEY
+                                {props.location}
                             </div>
-                            <a href="https://google.com" className="ml-5 inline-block align-middle underline text-gray-400">View on Google Maps</a>
+                            <a href={props.link} className="ml-5 inline-block align-middle underline text-gray-400">View on Google Maps</a>
                         </div>
                     </div>
-                    <div className="text-5xl font-semibold tracking-tighter">
-                        Archery
+                    <div className="text-5xl mt-4 font-semibold tracking-tighter">
+                        {props.title}
                     </div>
                     <div className="mt-9 font-semibold text-lg">
-                        Some tagline stuff
+                        {props.tag}
                     </div>
                     <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+                        {props.dscrp}
                     </div>
                 </div>
             </div>
-            <div className="border mt-5 mx-12">
-            </div>
+            <div className="border mt-5 mr-40"> </div>
         </div>
     );
 }
