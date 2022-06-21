95d286fed25a437fb7b1a151dab8e986e01eed81
Oliver Ye <oliverye@berkeley.edu>
2022-06-21 00:52:58 -0700

    more state practice

--- test/src/JokeCard.js
+++ test/src/JokeCard.js
@@ -1,18 +1,29 @@
 function JokeCard(props) {
+    let active;
+    active = props.on;
     return (
         <div>
-            <div className="text-xl bold mt-5">
-                {props.setup}
+            <div>
+                <button onClick={() => props.toggle(props.id)} className="border border-black border-2 rounded-md p-3 mt-4 hover:bg-white">
+                    Click me to toggle display status of the entire joke card
+                </button>
             </div>
-            {props.punchline != false &&
-            <div className="italic mb-5">
-                {props.punchline}
-            </div>
+            {active && 
+                <div>
+                    <div className="text-xl bold mt-5">
+                        {props.setup}
+                    </div>
+                    {props.punchline != false &&
+                    <div className="italic mb-5">
+                        {props.punchline}
+                    </div>
+                    }
+                    <div className="grid grid-cols-1 divide-y divide-indigo-500">
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
             }
-            <div class="grid grid-cols-1 divide-y divide-indigo-500">
-                <div></div>
-                <div></div>
-            </div>
         </div>
     );
 }
--- test/src/Main.js
+++ test/src/Main.js
@@ -1,13 +1,31 @@
 import JokeCard from "./JokeCard";
 import jokesData from "./jokesData";
+import React from "react";
 
 function Main() {
-    const jokeElements = jokesData.map((joke) => {
+    const [jokes, setJokes] = React.useState(jokesData);
+
+    const jokeElements = jokes.map((joke) => {
         return <JokeCard
+                key = {joke.id}
+                id = {joke.id}
                 setup = {joke.setup}
                 punchline = {joke.punchline}
+                on = {joke.on}
+                toggle = {toggleJoke}
                 />
     }) 
+
+
+    function toggleJoke(id) {
+        console.log(id);
+        setJokes(prevStatus => {
+            return prevStatus.map((element) => {
+                return element.id === id ? {...element, on: !element.on} : element
+            })
+        })
+
+    }
     return (
         <div>
             {jokeElements}
